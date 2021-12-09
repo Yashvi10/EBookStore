db189cf7483c27ed2ad2a365cd2fd8b1c4177bd6
Saloni Madhusudan Raythatha <sl768578@dal.ca>
2021-12-08 22:54:52 -0500

    updated some content on login

--- books_query.py
+++ books_query.py
@@ -5,8 +5,8 @@ import botocore.session
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb',
-                          aws_access_key_id='AKIAVVCE6YC6T32OQJGM',
-                          aws_secret_access_key='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',
+                          aws_access_key_id='AKIAVVCE6YC6UVS63U5J',
+                          aws_secret_access_key='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',
                           region_name='us-east-1')
 
 table = dynamodb.Table('library_storage')
