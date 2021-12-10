68d9c5e534ec156031174267eb43d455f4c449d3
Saloni Madhusudan Raythatha <sl768578@dal.ca>
2021-12-09 23:31:03 -0500

    Adding search functionality using dynamo, s3

--- app.py
+++ app.py
@@ -2,33 +2,61 @@
 
 from flask import Flask, render_template, redirect, url_for, request
 import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+import sys
+import cgi
+import cgitb
 
 app=Flask(__name__)
 
-# connection established with AWS congnito from python
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    print("0")
 
-    error = None
+
+
+# connection established with AWS congnito from python
+@app.route('/registration', methods=['GET', 'POST'])
+def login():
+    print(0.0)
     if request.method == 'POST':
-        client = boto3.client("cognito-idp", region_name="us-east-1")
-        response = client.sign_up(
-            ClientId='osdgke53n8m68qujsp2vocl01',
-            Username=request.form['email'],
-            Password=request.form['password'],
-            UserAttributes=[{"Name": "email", "Value": request.form['email']}], )
-        print(response)
-    return render_template('display_book_results.html', error=error)
+        if request.form.get("register_btn"):
+            print(1)
+            try:
+                print(2)
+                client = boto3.client("cognito-idp", region_name="us-east-1")
+                client.sign_up(
+                    ClientId='osdgke53n8m68qujsp2vocl01',
+                    Username=request.form['email'],
+                    Password=request.form['password'],
+                    UserAttributes=[{"Name": "email", "Value": request.form['email']}], )
+                print("======= User created")
+                print(3)
+                return render_template('display_book_results.html')
+            except ClientError as e:
+                print(4)
+                if e.response['Error']['Code'] == 'EntityAlreadyExists' or e.response['Error']['Code'] == 'UsernameExistsException':
+                    print(5)
+                    error = "User already exists - Do login instead"
+                    print("======= User already exists")
+                else:
+                    print(6)
+                    error = "Unexpected error"
+                    print("===== Unexpected error: %s" % e)
+                return render_template('/index.html', error=error)
+
+        elif request.form.get("login_btn"):
+            print(7)
+            return render_template('/display_book_results.html')
+
+
 
 
 #Route for handling the login page logic
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
+            return render_template('index.html', error=error)
         else:
             client = boto3.client("cognito-idp", region_name="us-east-1")
             response = client.sign_up(
@@ -37,20 +65,32 @@ def index():
                 Password=password,
                 UserAttributes=[{"Name": "email", "Value": email}], )
             print(response)
-            return redirect(url_for('home'))
-    return render_template('index.html', error=error)
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
 
-
-
-@app.route('/display_book_results.html', methods=['GET'])
+@app.route('/display_book_results.html', methods=['GET', 'POST'])
 def display_results():
     error = None
-    return render_template('display_book_results.html', error=error)
+    print(1)
+    dynamodb = boto3.resource('dynamodb',
+                              aws_access_key_id='AKIAVVCE6YC6UVS63U5J',
+                              aws_secret_access_key='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',
+                              region_name='us-east-1')
 
-def display():
-    return "This is about page";
-    return render_template('display_book_results.html', error=error)
-app.add_url_rule("/display_book_results", "display", display)
+    table = dynamodb.Table('library_storage2')
+    response = table.query(
+        KeyConditionExpression=Key('title').eq(request.form['search'])
+    )
+    items = response['Items']
+    print(items)
+    data = response['Items']
+    print(response)
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    return render_template('display_book_results.html', error=error, items=items)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- books_query.py
+++ books_query.py
@@ -4,22 +4,69 @@ import boto3
 import botocore.session
 from boto3.dynamodb.conditions import Key
 
-dynamodb = boto3.resource('dynamodb',
-                          aws_access_key_id='AKIAVVCE6YC6UVS63U5J',
-                          aws_secret_access_key='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',
-                          region_name='us-east-1')
+from flask import Flask, render_template, redirect, url_for, request
+import boto3
 
-table = dynamodb.Table('library_storage')
-response = table.query(
-    KeyConditionExpression=Key('year').eq(2019)
-)
-items = response['Items']
-print(items)
+app=Flask(__name__)
 
-data = response['Items']
-print(response)
-
-while 'LastEvaluatedKey' in response:
-    response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-    data.extend(response['Items'])
-
+# # displaying image content from s3 bucket
+#
+# client= boto3.client(
+#     's3',
+#     aws_access_key_id = 'AKIAVVCE6YC6UVS63U5J',
+#     aws_secret_access_key = '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',
+#     region_name = 'us-east-1'
+# )
+# s3 = boto3.resource('s3')
+# session = boto3.Session(
+#     aws_access_key_id="AKIAVVCE6YC6UVS63U5J",
+#     aws_secret_access_key="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",
+# )
+# session1=session.client('s3')
+# print(session1)
+# response = session1.list_buckets()
+#
+# # Output the bucket names
+# print('Existing buckets:')
+# for bucket in response['Buckets']:
+#     print(f'  {bucket["Name"]}')
+#
+# for key in session1.list_objects(Bucket='librarybucket2021')['Contents']:
+#     print(key['Key'])
+#
+#
+#
+#
+#
+#
+#
+# # connecting all dynamo db connect to website
+#
+# def list_files():
+#     # contents = []
+#     # response1 = session1.list_objects_v2(Bucket='tagsb00882283')
+#     # for image in response1.objects.all():
+#     #     contents.append(image.key)
+#     # return contents
+#     contents = []
+#     try:
+#         response1 = session1.list_objects_v2(Bucket='librarybucket2021')
+#         if 'Contents' in response1:
+#             for item in response1['Contents']:
+#                 # print(item)
+#                 contents.append(item)
+#         else:
+#             print('Bucket is empty')
+#     except ClientError as e:
+#         print(e)
+#
+#     return contents
+#
+#
+# @app.route('/', methods=['GET', 'POST'])
+# def files():
+#     list_of_files = list_files()
+#     return render_template('welcome.html', my_bucket='tagsb00882283', list_of_files=list_of_files)
+#
+#
+#
